edbb365f72aa82c14e1cdadb6a290d4cef37c7ac
LordAkirast <darkwarish@gmail.com>
2022-12-01 09:48:17 -0300

    Questions nearly finished. Trying to implement on click function

--- src/index.js
+++ src/index.js
@@ -7,15 +7,31 @@ import ReactDOM from 'react-dom/client'
 import "./index.css"
 
 
+const cards = [
+	{ question: "O que é JSX?", answer: "Uma extensão da linguagem JavaScript", number: "1"},
+	{ question: "O React é __", number: "2", answer: "Uma biblioteca JavaScript para construção de interfaces" },
+	{ question: "Componentes devem iniciar com __", number: "3", answer: "Letra maiúscula" },
+	{ question: "Podemos colocar __ dentro do JSX", number: "4", answer: "expressões" },
+]
+
+const setaStyle = {
+  width: "20px",
+  height: "23px"
+};
+
+let i = 0;
+
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
+
+  
   <>
   <div className='screen'>
     <div className='logo'>
     <img src="./assets/img/logo.png" alt="ZapRecall"/> ZapRecall
     </div>
     <div className='questions'>
-    <div className='question'>Pergunta 1</div>
+      {cards.map((card) => <div className='question'>Pergunta {card.number} <img style={setaStyle} src="./assets/img/seta_play.png" alt="Play"/></div> )}
     </div>
     
 
