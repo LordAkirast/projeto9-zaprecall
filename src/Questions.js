5449d459a928efd711afbcd42d3b652631790053
LordAkirast <darkwarish@gmail.com>
2022-12-04 02:04:33 -0300

    Buttons now changes if you choose the options of failure, nearly correct and correct. Need to implement function that changes with correct and incorrect.

--- src/Questions.js
+++ src/Questions.js
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 
 function Questions() {
-    const [clicado, setclicado] = useState(true)
+    const [clicado, setclicado] = useState(1)
     const cards = [
         { question: "O que é JSX?", answer: "Uma extensão da linguagem JavaScript", number: "1" },
         { question: "O React é __", number: "2", answer: "Uma biblioteca JavaScript para construção de interfaces" },
@@ -17,27 +17,63 @@ function Questions() {
     return (
 
         <>
-            <Question number="1" setaStyle={setaStyle} question="O que é JSX?" />
-            <Question number="2" setaStyle={setaStyle} question="O que é JMA?" />
+            <Question number="Pergunta 1" setaStyle={setaStyle} classCorrect="correct" classAlmost="almost" classWrong="wrong" classQ="question" classA="answer" question="O que significa 'emia'?" answer="'emia' significa Presente no Sangue." />
+            <Question number="Pergunta 2" setaStyle={setaStyle} classCorrect="correct" classAlmost="almost" classWrong="wrong" classQ="question" classA="answer" question="Qual é a música da vida?" answer="Silence, Brother." />
+            <Question number="Pergunta 3" setaStyle={setaStyle} classCorrect="correct" classAlmost="almost" classWrong="wrong" classQ="question" classA="answer" question="Um pêssego pode derrotar Tai Lung?" answer="Talvez possa, se você estiver disposto a treiná-lo." />
+            <Question number="Pergunta 4" setaStyle={setaStyle} classCorrect="correct" classAlmost="almost" classWrong="wrong" classQ="question" classA="answer" question="Quais são as formas de detectar um sonho lúcido?" answer="Olhar para as mãos. Espelhos. Teste de Realidade." />
         </>
     )
 }
 
 function Question(props) {
-    const [clicado, setclicado] = useState(true)
+    const [clicado, setclicado] = useState(0)
 
-    if (clicado === true) {
+    if (clicado === 0) {
         return (
-            <div className='question' onClick={() => setclicado(false)}>Pergunta {props.number} <img style={props.setaStyle} src="./assets/img/seta_play.png" alt="Play" /></div>
+            <div className={props.classQ} onClick={() => setclicado(1) & console.log(clicado)}>{props.number} <img style={props.setaStyle} src="./assets/img/seta_play.png" alt="Play" /></div>
 
         )
-    } else {
+    } else if (clicado === 1) {
         return (
-            <div className='question' onClick={() => setclicado(true)}>{props.question}</div>
+
+            <div className={props.classA} >{props.question} <img onClick={() => setclicado(2)} style={props.setaStyle} src="./assets/img/seta_virar.png" alt="Virar" /> </div>
         )
+    } else if (clicado === 2) {
+        return (
+            <>
+
+                <div className={props.classA} >{props.answer} <button className='dontRemember' onClick={() => setclicado(3)}>Não lembrei</button> <button onClick={() => setclicado(4)} className='almostRemember'>Quase Não lembrei</button> <button  onClick={() => setclicado(5)} className='Remember'>Zap</button>
+
+                </div>
+            </>
+        )
+
+    } else if (clicado === 3) {
+        return (
+            <>
+
+                <div className={props.classWrong} >{props.number} <img style={props.setaStyle} src="./assets/img/icone_erro.png" alt="erro" />  </div>
+            </>
+        )
+
+    } else if (clicado === 4) {
+        return (
+            <>
+
+                <div className={props.classAlmost} >{props.number} <img style={props.setaStyle} src="./assets/img/icone_quase.png" alt="quase" />  </div>
+            </>
+        )
+
+    } else if (clicado === 5) {
+        return (
+            <>
+
+                <div className={props.classCorrect} >{props.number} <img style={props.setaStyle} src="./assets/img/icone_certo.png" alt="certo" />  </div>
+            </>
+        )
+
     }
 }
-
 
 
 export default Questions
